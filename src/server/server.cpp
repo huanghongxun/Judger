b84c5ee548b1f1ae21a35fbd4d884ab5ffa06c0d
Yuhui Huang <i@huangyuhui.net>
2019-06-27 10:56:46 +0800

    Limit queued tasks

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,5 +1,6 @@
 #include "server/server.hpp"
 #include <glog/logging.h>
+#include <thread>
 #include "common/messages.hpp"
 
 namespace judge::server {
@@ -23,6 +24,10 @@ static map<unsigned, submission> submissions;
 static map<unsigned, vector<judge::message::task_result>> task_results;
 static map<unsigned, size_t> finished_task_cases;
 
+static unsigned queued_tasks = 0;
+// TODO: configurable
+static const unsigned MAX_QUEUED_TASKS = 2 * std::thread::hardware_concurrency();
+
 submission &get_submission_by_judge_id(unsigned judge_id) {
     return submissions.at(judge_id);
 }
@@ -41,11 +46,14 @@ void summarize(unsigned judge_id) {
 
 void server(message::queue &testcase_queue, message::queue &result_queue) {
     while (true) {
+        // 优先清空 result_queue
         while (true) {
             // 从结果队列中获取评测客户端的评测结果，并更新对应 submission 的状态
             judge::message::task_result task_result;
             auto envelope = result_queue.recv_from_pod(task_result, message::queue::NO_WAIT);
             if (envelope.success) {
+                --queued_tasks;
+
                 unsigned judge_id = task_result.judge_id;
                 if (!submissions.count(judge_id)) {
                     LOG(ERROR) << "Test case exceeded [judge_id: " << judge_id << "]";
@@ -59,13 +67,18 @@ void server(message::queue &testcase_queue, message::queue &result_queue) {
                     task_results[0] = task_result;
                 }
 
+                DLOG(INFO) << "Testcase [" << submit.category << "-" << submit.prob_id << "-" << submit.sub_id
+                           << ", status: " << (int)task_result.status << ", runtime: " << task_result.run_time
+                           << ", memory: " << task_result.memory_used << ", run_dir: " << task_result.run_dir
+                           << ", data_dir: " << task_result.data_dir << "]";
+
                 size_t finished = ++finished_task_cases[judge_id];
                 // 如果当前提交的所有测试点都完成测试，则返回评测结果
                 if (finished == submit.test_cases.size()) {
                     summarize(judge_id);
                     continue;  // 跳过本次评测过程
                 } else if (finished > submit.test_cases.size()) {
-                    LOG(ERROR) << "Test case exceeded [category: " << submit.category << ", prob: " << submit.prob_id << ", sub: " << submit.sub_id << "]";
+                    LOG(ERROR) << "Test case exceeded [" << submit.category << "-" << submit.prob_id << "-" << submit.sub_id << "]";
                     continue;  // 跳过本次评测过程
                 }
 
@@ -82,6 +95,7 @@ void server(message::queue &testcase_queue, message::queue &result_queue) {
                                 .id = (uint8_t)i,
                                 .type = test_case.check_type};
                             testcase_queue.send_as_pod(client_task);
+                            ++queued_tasks;
                         }
                     } else {
                         // 否则评测直接结束，返回评测结果
@@ -94,12 +108,17 @@ void server(message::queue &testcase_queue, message::queue &result_queue) {
             }
         }
 
-        {
+        // 限制拉取提交的总数
+        // TODO: 将拉取了的提交保存到本地持久化以便评测系统挂掉后重启可以 rejudge
+        // TODO: 主动 fetch 未评测的数据
+        if (queued_tasks < MAX_QUEUED_TASKS) {
             // 尝试从服务器拉取提交，如果拉取到了，那么生成一个编译任务发送给评测客户端
 
             for (auto &[category, server] : judge_servers) {
                 judge::server::submission submission;
                 if (server->fetch_submission(submission)) {
+                    LOG(INFO) << "Judging submission [" << submission.category << "-" << submission.prob_id << "-" << submission.sub_id << "]";
+
                     bool compile_case_valid = false;
                     for (size_t i = 0; i < submission.test_cases.size(); ++i) {
                         auto &test_case = submission.test_cases[i];
@@ -112,7 +131,7 @@ void server(message::queue &testcase_queue, message::queue &result_queue) {
                     }
 
                     if (!compile_case_valid) {
-                        LOG(WARNING) << "Submission from [" << submission.category << ", " << submission.prob_id << ", " << submission.sub_id << "] is not valid";
+                        LOG(WARNING) << "Submission from [" << submission.category << "-" << submission.prob_id << "-" << submission.sub_id << "] is not valid";
                         continue;
                     }
 
@@ -143,6 +162,8 @@ void server(message::queue &testcase_queue, message::queue &result_queue) {
                         .id = judge::message::client_task::COMPILE_ID,
                         .type = judge::message::client_task::COMPILE_TYPE};
                     testcase_queue.send_as_pod(client_task);
+
+                    ++queued_tasks;
                 }
             }
         }
