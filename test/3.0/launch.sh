e0642877cba7a2bb78d9b02bb048b12905a142d8
Yuhui Huang <i@huangyuhui.net>
2019-07-17 15:20:11 +0800

    move test files

---
+++ test/3.0/launch.sh
@@ -0,0 +1,1 @@
+../bin/judge-system --enable-3 config-moj.json --client 1 --exec-dir ~/Judger/exec/ --cache-dir /tmp --run-diir /tmp --chroot-dir /chroot --cache-random-data 100 --run-user matrix4 --run-group matrix4
